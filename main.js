d50c38fb5c2b597d3b9f2f700fb67b3e9de7d40d
Elisa <elisa.torazza@gmail.com>
2020-09-02 15:58:35 +0200

    Tentativo di utilizzo della tastiera

--- main.js
+++ main.js
@@ -3,14 +3,35 @@ $(document).ready(function() {
 
 $(".next").click(
   function() {
-    var removeActive =$(".slider-wrapper .images .active");
+    var removeActive = $(".slider-wrapper .images .active");
     removeActive.removeClass("active");
     removeActive.next().addClass("active");
     if (removeActive.hasClass("last")) {
       $(".slider-wrapper .images .first").addClass("active");
     }
   }
-)
+);
+
+
+$(".prev").click(
+  function() {
+    var removeActive = $(".slider-wrapper .images .active");
+    removeActive.removeClass("active");
+    removeActive.prev().addClass("active");
+    if (removeActive.hasClass("first")) {
+      $(".slider-wrapper .images .last").addClass("active");
+    }
+  }
+
+);
+
+$(document).keydown(
+  function() {
+    if (e.keyCode == 37) {
+    alert("left arrow");
+  }
+}
+);
 
 
 
